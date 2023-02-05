860ec7405a9297b007a9c77823d1536398fb631a
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-02-05 16:41:26 +0300

    Update catalogue page

--- src/main/java/com/tish/servlets/CatalogueServlet.java
+++ src/main/java/com/tish/servlets/CatalogueServlet.java
@@ -1,5 +1,6 @@
 package com.tish.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,36 +14,24 @@ import com.tish.dao.TshirtDAO;
 import com.tish.database.DatabaseConnection;
 import com.tish.entities.Tshirt;
 
-/**
- * Servlet implementation class CatalogueServlet
- */
 @WebServlet("/catalogue")
 public class CatalogueServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public CatalogueServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		TshirtDAO tshirtDAO = new TshirtDAO(DatabaseConnection.getInstance());
 		List<Tshirt> liste_tshirt = tshirtDAO.getAll();
 		request.setAttribute("liste_tshirt", liste_tshirt);
+		String uploadPath = getServletContext().getContextPath() + File.separator + "uploads" + File.separator;
+		request.setAttribute("upload_path", uploadPath);
 		request.getRequestDispatcher("/WEB-INF/jsp/pages/catalogue.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- src/main/webapp/WEB-INF/jsp/pages/catalogue.jsp
+++ src/main/webapp/WEB-INF/jsp/pages/catalogue.jsp
@@ -1,12 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ page
+	import="java.util.List, com.tish.entities.Tshirt, java.util.ArrayList"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<link rel="stylesheet"
+	href="${ pageContext.request.contextPath }/ressources/styles/pico.min.css">
+<link rel="stylesheet"
+	href="${ pageContext.request.contextPath }/ressources/styles/style.css">
+<title>tish | Catalogue</title>
 </head>
 <body>
-
+	<jsp:include page='../composants/navbar.jsp' />
+	<main class='container'>
+		<section id="shirt-catalog">
+			<%
+			List<Tshirt> liste_tshirt = (ArrayList<Tshirt>) request.getAttribute("liste_tshirt");
+			%>
+			<%
+			for (Tshirt tshirt : liste_tshirt) {
+			%>
+			<article class="shirt-item">
+				<img
+					src="<%=(String) request.getAttribute("upload_path") + tshirt.getMotif()%>"
+					alt="<%=tshirt.getMotif()%>" />
+					<div class='tshirt-color' style='background-color: <%=tshirt.getCouleur()%>'></div>
+				<footer>
+					<span><%= tshirt.getPrix()%> Ar</span>
+					<span><%= tshirt.getTaille() %></span>
+					<div class="grid-fluid">
+						<a href="ajouter_dans_panier?idTshirt=<%=tshirt.getIdTShirt()%>"
+							role='button'>Ajouter dans le panier</a>
+					</div>
+				</footer>
+			</article>
+			<%
+			}
+			%>
+		</section>
+	</main>
+	<jsp:include page='../composants/footer.jsp' />
 </body>
 </html>
