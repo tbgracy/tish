3daf29cc4ada9a8854a81377206736eb2d122bbf
j03-dev <24nomeniavo@gmail.com>
2023-02-05 15:33:51 +0300

    Add:  get all t-shirt for catalogue

--- src/main/java/com/tish/servlets/CatalogueServlet.java
+++ src/main/java/com/tish/servlets/CatalogueServlet.java
@@ -1,11 +1,17 @@
 package com.tish.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.tish.dao.TshirtDAO;
+import com.tish.database.DatabaseConnection;
+import com.tish.entities.Tshirt;
 
 /**
  * Servlet implementation class CatalogueServlet
@@ -26,7 +32,9 @@ public class CatalogueServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		TshirtDAO tshirtDAO = new TshirtDAO(DatabaseConnection.getInstance());
+		List<Tshirt> liste_tshirt = tshirtDAO.getAll();
+		request.setAttribute("liste_tshirt", liste_tshirt);
 		request.getRequestDispatcher("/WEB-INF/jsp/pages/catalogue.jsp").forward(request, response);
 	}
 
