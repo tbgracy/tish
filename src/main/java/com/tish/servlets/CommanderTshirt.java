f15b71dabdf892cffddae7290b4eaf4a19d8e40c
j03-dev <24nomeniavo@gmail.com>
2023-02-21 00:00:28 +0300

    Update  quantite tish after commande

--- src/main/java/com/tish/servlets/CommanderTshirt.java
+++ src/main/java/com/tish/servlets/CommanderTshirt.java
@@ -12,9 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.tish.dao.CommandeDAO;
+import com.tish.dao.TshirtDAO;
 import com.tish.dao.TshirtItemDAO;
 import com.tish.database.DatabaseConnection;
 import com.tish.entities.Commande;
+import com.tish.entities.Tshirt;
 import com.tish.entities.TshirtItem;
 import com.tish.entities.User;
 
@@ -23,6 +25,7 @@ public class CommanderTshirt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private CommandeDAO commandeDAO = new CommandeDAO(DatabaseConnection.getInstance());
 	private TshirtItemDAO tshirtItemDAO = new TshirtItemDAO(DatabaseConnection.getInstance());
+	private TshirtDAO tishDAO = new TshirtDAO(DatabaseConnection.getInstance());
 
 	public CommanderTshirt() {
 		super();
@@ -34,17 +37,26 @@ public class CommanderTshirt extends HttpServlet {
 		User user = (User) session.getAttribute("user");
 
 //		@SuppressWarnings("unchecked")
-		List<TshirtItem> pannier = (List<TshirtItem>) session.getAttribute("pannier");
-		
-		List<TshirtItem> pannier_vide = new ArrayList<TshirtItem>();
-		
-		if (pannier != null) {
-			for (TshirtItem item : pannier) {
+		List<TshirtItem> panier = (List<TshirtItem>) session.getAttribute("pannier");
+
+		List<TshirtItem> panier_vide = new ArrayList<TshirtItem>();
+
+		if (panier != null) {
+			for (TshirtItem item : panier) {
 				Commande commande = new Commande(user.getIdUtilisateur());
 				if (commandeDAO.create(commande)) {
 					item.setIdCmd(commande.getIdCmd());
 					if (tshirtItemDAO.create(item)) {
-						session.setAttribute("pannier", pannier_vide);
+
+						int quantiteCmd = item.getQuantite();
+						Tshirt tish = tishDAO.find(item.getIdTshirt());
+						int nombreUpdate = tish.getNombre() - quantiteCmd;
+
+						tish.setNombre(nombreUpdate);
+
+						tishDAO.update(tish);
+
+						session.setAttribute("pannier", panier_vide);
 						response.sendRedirect("catalogue");
 						return;
 					}
