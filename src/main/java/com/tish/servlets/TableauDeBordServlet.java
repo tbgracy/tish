7195bf264939bc05d9434b23deef71c1a6cec683
j03-dev <24nomeniavo@gmail.com>
2023-02-21 00:03:19 +0300

    Add nombre commnade to tashboard

--- src/main/java/com/tish/servlets/TableauDeBordServlet.java
+++ src/main/java/com/tish/servlets/TableauDeBordServlet.java
@@ -7,15 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tish.dao.CommandeDAO;
+import com.tish.database.DatabaseConnection;
+
 @WebServlet("/tableau_de_bord")
 public class TableauDeBordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private CommandeDAO commandeDAO = new CommandeDAO(DatabaseConnection.getInstance());
        
     public TableauDeBordServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int nombreCommande = commandeDAO.getAll().size();
+		request.setAttribute("nombreCmd", nombreCommande);
 		request.getRequestDispatcher("/WEB-INF/jsp/pages/tableau_de_bord.jsp").forward(request, response);
 	}
 
