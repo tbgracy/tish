47a4dcd5777af2481db2385ec419b40b6b2c0c38
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-02-20 22:34:16 +0300

    Display error message on signup

--- src/main/java/com/tish/servlets/InscriptionServlet.java
+++ src/main/java/com/tish/servlets/InscriptionServlet.java
@@ -30,6 +30,11 @@ public class InscriptionServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String pseudo = request.getParameter("pseudo");
 		String motDePasse = request.getParameter("motdepasse");
+		String motDePasseConf = request.getParameter("motdepasseconf");
+		if (!motDePasse.equals(motDePasseConf)) {
+			response.sendRedirect("inscription?message=Les mots de passe doivent etre les memes.");
+			return;
+		}
 		String numeroTelephone = request.getParameter("numerotel");
 		UserDAO userDAO = new UserDAO(DatabaseConnection.getInstance());
 		User newUser = new User(pseudo, motDePasse, numeroTelephone);
@@ -38,8 +43,7 @@ public class InscriptionServlet extends HttpServlet {
 			this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/pages/inscription.jsp").forward(request,
 					response);
 		} else {
-			response.getWriter().append("Error creating user " + pseudo);
-			doGet(request, response);
+			response.sendRedirect("inscription?message=Erreur lors de l'ajout de l'utilisateur " + pseudo + ".<br> Veuillez verifier les informations entrees.");
 		}
 	}
 
